b2c5ef63540a68244a5037b53a16593daadf3662
eevvvaa <144210629+eevvvaa@users.noreply.github.com>
2023-09-30 19:52:21 +0800

    Add files via upload

---
+++ 4112029030_HW4.py
@@ -0,0 +1,40 @@
+import sqlite3
+conn=sqlite3.connect('BBQ.db')
+cursor=conn.cursor()
+cursor.execute('''
+      CREATE TABLE IF NOT EXISTS meat(
+             id INTEGER PRIMARY KEY,
+             name TEXT,
+             price INTEGER,
+             quantity INTEGER
+     )''')
+
+cursor.execute("INSERT INTO meat (name, price, quantity) VALUES ('chicken', 30, 5)")
+cursor.execute("INSERT INTO meat (name, price, quantity) VALUES ('beaf', 55, 10)")
+cursor.execute("INSERT INTO meat (name, price, quantity) VALUES ('pork', 40, 15)")
+conn.commit()
+cursor.execute("SELECT * FROM meat")
+meat=cursor.fetchall()
+print("烤肉列表:")
+for BBQ in meat:
+    print(BBQ)
+
+cursor.execute("UPDATE meat SET price=35 WHERE name='pork' ")
+cursor.execute("UPDATE meat SET quantity=30 WHERE name='chicken' ")
+conn.commit()
+cursor.execute("SELECT * FROM meat")
+meat=cursor.fetchall()
+print("烤肉列表:")
+for BBQ in meat:
+    print(BBQ)
+
+cursor.execute("DELETE FROM meat WHERE price=40 ")
+conn.commit()
+cursor.execute("SELECT * FROM meat")
+meat=cursor.fetchall()
+print("烤肉列表:")
+for BBQ in meat:
+    print(BBQ)
+
+cursor.close()
+conn.close()
